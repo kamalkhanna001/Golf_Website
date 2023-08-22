fa8b9d4d5f1c3e38a9932eb17875ddf33a0f6a3b
kamal khanna <khanank819@gmail.com>
2023-08-22 10:47:06 +0530

    More Modification

--- Projects/Golf_Website/script.js
+++ Projects/Golf_Website/script.js
@@ -2,13 +2,25 @@ var crsr = document.querySelector("#cursor");
 var blur = document.querySelector("#cursorblur");
 
 document.addEventListener("mousemove", function (dets) {
-  crsr.style.left = dets.x + "px";
+  crsr.style.left = dets.x +35 + "px";
   crsr.style.top = dets.y  + "px";
   blur.style.left = dets.x - 150 + "px";
   blur.style.top = dets.y - 150 + "px";
 });
 
-
+var h4all = document.querySelectorAll("#nav h4");
+h4all.forEach(function (elem) {
+  elem.addEventListener("mouseenter", function () {
+    crsr.style.scale = 4;
+    crsr.style.border = "1px solid #fff";
+    crsr.style.backgroundColor = "transparent";
+  });
+  elem.addEventListener("mouseleave", function () {
+    crsr.style.scale = 1;
+    crsr.style.border = "0px solid #95C11E";
+    crsr.style.backgroundColor = "#95C11E";
+  });
+});
 
 // YE SCROLL TRIGGER HAI ISSE NAV BAR KI HEIGHT OR BG COLOR SCROLL KE WAQT CHANGE HO JAEGA 
 gsap.to("#nav", {
@@ -34,5 +46,30 @@ gsap.to("#nav", {
       start: "top -25%",
       end: "top -70%",
       scrub: 2,
+    },
+  });
+
+  gsap.from("#colon1", {
+    y: -70,
+    x: -70,
+    scrollTrigger: {
+      trigger: "#colon1",
+      scroller: "body",
+      // markers:true,
+      start: "top 55%",
+      end: "top 45%",
+      scrub: 4,
+    },
+  });
+  gsap.from("#colon2", {
+    y: 70,
+    x: 70,
+    scrollTrigger: {
+      trigger: "#colon1",
+      scroller: "body",
+      // markers:true,
+      start: "top 55%",
+      end: "top 45%",
+      scrub: 4,
     },
   });
